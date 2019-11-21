543f022a7988021afb3379548f52e983a78de4c9
Alex Nik <alexnik@MacBook-Air-Alex.local>
2019-11-21 12:38:06 +0200

    Refactor code

--- src/app/components/TodoListItem.js
+++ src/app/components/TodoListItem.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
-import { TodoButton, TodoDelButton } from '@todo';
+import { TodoDelButton } from '@todo';
 import { withTranslation } from "react-i18next";
 import { connect } from 'react-redux';
 import { deletItem } from 'store/action/actions.payload';
